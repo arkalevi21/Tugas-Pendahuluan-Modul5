f31c3f036706f48de824a3d10bb952b50ec6bd48
Ar Kalevi <arkalevi@MacBook-Air-Ar.local>
2026-03-30 07:21:16 +0700

    menambahkan class DataGeneric

--- tpmodul5_103082400051/Program.cs
+++ tpmodul5_103082400051/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+DataGeneric<string> dataNIM = new DataGeneric<string>("103082400051"); // Isi NIM kamu 
+dataNIM.PrintData();
+
+public class DataGeneric<T> {
+    private T data;
+    public DataGeneric(T data) { this.data = data; }
+    public void PrintData() {
+        Console.WriteLine($"Data yang tersimpan adalah: {this.data}");
+    }
+}
